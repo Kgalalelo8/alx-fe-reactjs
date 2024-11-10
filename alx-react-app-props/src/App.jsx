d0bdad69df48d87adb6cd08bb98f20c91fc6f3fc
Kgalalelo <tshenoloo1095@gmail.com>
2024-11-11 00:26:45 +0200

    Saved

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from './Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import About from './About';
+import About from './components/About';
 import Services from './Services';
 import Contact from './Contact';
 import Navbar from './Navbar';
