00b7d7ceb0ef82fd23d53cf3344cf36f7cead7c0
Kgalalelo <tshenoloo1095@gmail.com>
2024-11-10 22:01:09 +0200

    added inline styling to components

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1 style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}  >My Favorite Cities</h1>
+        </header>
+    );
+}
+
+export default Header;
+
+
+
+
+
+
+
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: '#f0f0f0', padding: '20px', textAlign: 'center' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
+
+
+
+
