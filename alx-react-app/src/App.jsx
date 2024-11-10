4bbe52c0c015189443cd22c560eb300b26258dd8
Kgalalelo <tshenoloo1095@gmail.com>
2024-11-10 21:21:48 +0200

    added header.jsx footer.jsx maincontent.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,24 @@
-import WelcomeMessage from './WelcomeMessage';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './WelcomeMessage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,6 +39,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
