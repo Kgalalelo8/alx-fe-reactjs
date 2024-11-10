7eb16db4e358bfc14e6345d4a95a2fcacdf3f4be
Kgalalelo <tshenoloo1095@gmail.com>
2024-11-11 00:04:06 +0200

    added new files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import Footer from './Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
 
 function App() {
-    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
-    return (
-        <UserContext.Provider value={userData}>
-            <ProfilePage />
-        </UserContext.Provider>
-    );
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ alx-react-app-props/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', textAlign: 'center', backgroundColor: '#333', color: '#fff' }}>
+        © 2023 My Company
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
