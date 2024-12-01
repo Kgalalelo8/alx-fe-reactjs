42817a94400d5d74f27aa7da49895d6102e38455
Kgalalelo <tshenoloo1095@gmail.com>
2024-12-02 00:02:03 +0200

    ...

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,6 @@
-// src/components/UserProfile.jsx
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
@@ -10,7 +9,7 @@ function UserProfile() {
         <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center">
           John Doe
         </h1>
-        <p className="text-sm sm: p-4 sm:text-base text-gray-600 text-center">
+        <p className="text-sm sm:text-base text-gray-600 text-center">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
